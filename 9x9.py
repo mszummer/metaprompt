a9fc152519dee2e834d174655d71d074418beb56
Marc Fischer <mail@marcfischer.at>
2024-06-29 17:45:45 -0700

    basic meta prompting

--- 9x9.py
+++ 9x9.py
@@ -4,6 +4,19 @@ import sudokum
 import numpy as np
 import re
 
+def llm(prompt, verbose=False):
+    if verbose: print(f"> {prompt}")
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[{"role": "user",
+                   "content": prompt 
+                   }],
+    )
+    answer = completion.choices[0].message.content
+    if verbose: print(f"< {answer}")
+    return answer
+
+
 def format_sudoku(puzzle):
     return str(np.array(puzzle))
 
@@ -17,9 +30,7 @@ def parse_sudoku(text):
         
 
 client = OpenAI()
-def run_llm(puzzle, few_shot=0):
-    prompt = "Solve this 9x9 sudoku by replacing 0s with numbers 1 to 9."
-    prompt += "Only print the solution, no other text.\n\n"
+def run_llm(puzzle, prompt, few_shot=0):
     for _ in range(few_shot):
         p = sudokum.generate(mask_rate=0.1)
         _, s = sudokum.solve(p)
@@ -31,28 +42,30 @@ def run_llm(puzzle, few_shot=0):
         prompt += "\n\n"
 
     prompt += "The sudoku is:" + "\n\n" + format_sudoku(puzzle)
-    completion = client.chat.completions.create(
-        model="gpt-4",
-        messages=[{"role": "user",
-                   "content": prompt 
-                   }],
-    )
-    answer = completion.choices[0].message.content
+    answer = llm(prompt)
     answer = parse_sudoku(answer)
     return answer
 
 
-N = 10
+N = 25
 cnt_correct = 0
 cnt = 0
 accuracy = 0
 iterator = trange(N)
+
+prompt = """Solve this 9x9 sudoku by replacing 0s with numbers 1 to 9.
+            Only print the solution, no other text.\n\n"""
+
+metaprompt = f"Refine the following instructions for a LLM solving a sudoku puzzle. Make sure to make it better at solving the task. ```{prompt}```"
+
+prompt = llm(metaprompt, verbose=True)
+
 for line in iterator:
-    puzzle = sudokum.generate(mask_rate=0.275)
+    puzzle = sudokum.generate(mask_rate=0.3)
     _, solution = sudokum.solve(puzzle)
     puzzle, solution = np.array(puzzle), np.array(solution)
     
-    answer = run_llm(puzzle, few_shot=1)
+    answer = run_llm(puzzle, prompt=prompt, few_shot=0)
     correct = np.all(answer == solution)
     cnt_correct += correct
     cnt += 1
